fc978ac074b3b1cc14271b8e11cc1f1fb8527a06
nu11secur1ty <systadmi@abv.bg>
2016-12-11 18:35:25 +0200

    Update python_installer.sh

--- python_installer.sh
+++ python_installer.sh
@@ -6,9 +6,9 @@ yum install -y openssl-devel
 yum install -y ncurses-devel 
 yum install -y sqlite-devel 
     cd /opt
-       wget --no-check-certificate https://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz
-       tar xf Python-2.7.6.tar.xz
-cd Python-2.7.6
+       wget --no-check-certificate https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tar.xz
+       tar xf Python-2.7.12.tar.xz
+cd Python-2.7.12
 ./configure --prefix=/usr/local
  make && sudo make altinstall
 echo "alias python=/usr/local/bin/python2.7" >> ~/.bashrc
