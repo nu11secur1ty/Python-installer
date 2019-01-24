67f8492c7fa913d51ba6a716ae0df8266f2a7453
nu11secur1ty <venvaropt@gmail.com>
2019-01-24 16:16:08 +0200

    Update python37installer.sh

--- python37installer.sh
+++ python37installer.sh
@@ -7,6 +7,8 @@ zypper in -y bzip2
 zypper in -y openssl-devel
 zypper in -y ncurses-devel 
 zypper in -y sqlite-devel 
+zypper in zlib-devel bzip2 libbz2-devel libffi-devel libopenssl-devel readline-devel sqlite3 sqlite3-devel xz xz-devel 
+
     cd /opt
        wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
        tar -xvf Python-3.7.2.tgz
