0c564fa60407b6f57e1ff603cbd08165d1c2040f
nu11secur1ty <venvaropt@gmail.com>
2019-01-24 15:09:45 +0200

    Update python37installer.sh

--- python37installer.sh
+++ python37installer.sh
@@ -5,9 +5,9 @@ zypper install -y openssl-devel
 zypper install -y ncurses-devel 
 zypper install -y sqlite-devel 
     cd /opt
-       wget --no-check-certificate https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tar.xz
-       tar xf Python-3.7.2.tar.xz
-cd Python-3.7.2.tar.xz
+       wget --no-check-certificate https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+       tar xf Python-3.7.2.tgz
+cd Python-3.7.2
 ./configure --prefix=/usr/local
 make && make install
 exit 0;
