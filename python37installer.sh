e5826eba3ae87446a917e2b0d082859107d60254
nu11secur1ty <venvaropt@gmail.com>
2019-01-24 15:04:07 +0200

    Update python37installer.sh

--- python37installer.sh
+++ python37installer.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/bash
-yum groupinstall "Development tools"
-yum install -y zlib-devel
-yum install -y bzip2-devel
-yum install -y openssl-devel
-yum install -y ncurses-devel 
-yum install -y sqlite-devel 
+zypper install -y zlib-devel
+zypper install -y bzip2-devel
+zypper install -y openssl-devel
+zypper install -y ncurses-devel 
+zypper install -y sqlite-devel 
     cd /opt
-       wget --no-check-certificate https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tar.xz
-       tar xf Python-2.7.12.tar.xz
-cd Python-2.7.12
+       wget --no-check-certificate https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tar.xz
+       tar xf Python-3.7.2.tar.xz
+cd Python-3.7.2.tar.xz
 ./configure --prefix=/usr/local
-make && make altinstall
-echo "alias python=/usr/local/bin/python2.7" >> ~/.bashrc
-echo "export PATH=/usr/local/bin/python2.7:\$PATH" >> ~/.bashrc
-source /root/.bashrc
+make && make install
 exit 0;
