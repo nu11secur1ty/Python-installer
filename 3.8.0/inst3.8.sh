9a6213a428cceea0a74d3a829e5451f6ae401525
nu11secur1ty <venvaropt@gmail.com>
2019-11-29 13:57:09 +0200

    Update inst3.8.sh

--- 3.8.0/inst3.8.sh
+++ 3.8.0/inst3.8.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/bash
 # Author Ventsislav Varbanovski @nu11secur1ty
-# Support developer Atanas Stoev
 # @2019
 
-zypper install -y gcc
-zypper install -y make
-zypper install -y zlib-devel
+apt install -y gcc
+apt install -y make
+apt install -y zlib-devel
 #zypper in -y bzip2-devel
-zypper install -y bzip2
-zypper install -y openssl-devel
-zypper install -y ncurses-devel 
-zypper install -y sqlite-devel 
-zypper install -y zlib-devel bzip2 libbz2-devel libffi-devel libopenssl-devel readline-devel sqlite3 sqlite3-devel xz xz-devel 
+apt install -y bzip2
+apt install -y openssl-devel
+apt install -y ncurses-devel 
+apt install -y sqlite-devel 
+apt install -y zlib-devel bzip2 libbz2-devel libffi-devel libopenssl-devel readline-devel sqlite3 sqlite3-devel xz xz-devel 
 
     cd /opt
-       wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
-       tar -xvf Python-3.7.2.tgz
-cd Python-3.7.2
+       wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
+       tar -xvf Python-3.8.0.tgz
+cd Python-3.8.0
 ./configure --prefix=/usr/local
     make  
     make install
         sleep 3
-    python3.7 -m pip install --upgrade setuptools
-    python3.7 -m pip install --upgrade pip
+    python3.8 -m pip install --upgrade setuptools
+    python3.8 -m pip install --upgrade pip
         sleep 1
-        echo -e "Congratulations. Now you are using python 3.7 on OpenSuse 42.3 and 15.0.\n\nIf somehow you see an error please report it on our official GitHub page.\n\nhttps://github.com/nu11secur1ty/Python-installer/issues \n\n <--||===========>"
+        echo -e "Congratulations. Now you are using python 3.8 on KaliLinux 2019.4.\n\nIf somehow you see an error please report it on our official GitHub page.\n\nhttps://github.com/nu11secur1ty/Python-installer/issues \n\n <--||===========>"
 
 exit 0;
