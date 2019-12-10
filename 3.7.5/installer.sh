45a0a3a5e073fd9f18b0da4a93e55b24b6ae530c
nu11secur1ty <venvaropt@gmail.com>
2019-12-10 13:31:58 +0200

    Update installer.sh

--- 3.7.5/installer.sh
+++ 3.7.5/installer.sh
@@ -1,1 +1,29 @@
-Python-3.7.5.tgz
+#!/usr/bin/bash
+# Author Ventsislav Varbanovski @nu11secur1ty
+# Support developer Atanas Stoev
+# @2019
+
+zypper install -y gcc
+zypper install -y make
+zypper install -y zlib-devel
+#zypper in -y bzip2-devel
+zypper install -y bzip2
+zypper install -y openssl-devel
+zypper install -y ncurses-devel 
+zypper install -y sqlite-devel 
+zypper install -y zlib-devel bzip2 libbz2-devel libffi-devel libopenssl-devel readline-devel sqlite3 sqlite3-devel xz xz-devel 
+
+    cd /opt
+       wget https://www.python.org/ftp/python/3.7.5/Python-3.7.5.tgz
+       tar -xvf Python-3.7.5.tgz
+cd Python-3.7.5
+./configure --prefix=/usr/local
+    make  
+    make install
+        sleep 3
+    python3.7 -m pip install --upgrade setuptools
+    python3.7 -m pip install --upgrade pip
+        sleep 1
+        echo -e "Congratulations. Now you are using python 3.7 on OpenSuse 42.3 and 15.0.\n\nIf somehow you see an error please report it on our official GitHub page.\n\nhttps://github.com/nu11secur1ty/Python-installer/issues \n\n <--||===========>"
+
+exit 0;
