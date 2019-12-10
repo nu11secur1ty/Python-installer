871cc677abddf42f32dc7c192e78e29056419a5d
nu11secur1ty <venvaropt@gmail.com>
2019-12-10 12:03:39 +0200

    Create installer.sh

---
+++ Suse/3.8.0/installer.sh
@@ -0,0 +1,30 @@
+ 
+#!/usr/bin/bash
+# Author Ventsislav Varbanovski @nu11secur1ty
+# Support developer Atanas Stoev
+# @2019
+
+zypper install -y gcc
+zypper install -y make
+zypper install -y zlib-devel
+#zypper in -y bzip2-devel
+zypper install -y bzip2
+zypper install -y openssl-devel
+zypper install -y ncurses-devel 
+zypper install -y sqlite-devel 
+zypper install -y zlib-devel bzip2 libbz2-devel libffi-devel libopenssl-devel readline-devel sqlite3 sqlite3-devel xz xz-devel 
+
+    cd /opt
+       wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
+       tar -xvf Python-3.8.0.tgz
+cd Python-3.8.0
+./configure --prefix=/usr/local
+    make  
+    make install
+        sleep 3
+    python3.8 -m pip install --upgrade setuptools
+    python3.8 -m pip install --upgrade pip
+        sleep 1
+        echo -e "Congratulations. Now you are using python 3.7 on OpenSuse 42.3 and 15.0.\n\nIf somehow you see an error please report it on our official GitHub page.\n\nhttps://github.com/nu11secur1ty/Python-installer/issues \n\n <--||===========>"
+
+exit 0;
