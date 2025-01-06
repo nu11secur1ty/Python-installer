33e7a120228488ce18c1c95c7ef914d3b8a212ab
nu11secur1ty <venvaropt@gmail.com>
2025-01-06 15:46:27 +0200

    Update all_py.sh

--- customization/all_py.sh
+++ customization/all_py.sh
@@ -1,1 +1,49 @@
-### By nu11secur1ty
+#!/usr/bin/bash
+# @n11secur1ty
+
+# Packages
+sudo apt-get update
+sudo apt-get install \
+    curl \
+    gcc \
+    libbz2-dev \
+    libev-dev \
+    libffi-dev \
+    libgdbm-dev \
+    liblzma-dev \
+    libncurses-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    make \
+    tk-dev \
+    wget \
+    zlib1g-dev
+
+# Env
+echo "Give the version"
+read VERSION
+echo "Give the major"
+read MAJOR
+export PYTHON_VERSION=$VERSION
+export PYTHON_MAJOR=$MAJOR
+
+# Get
+curl -O https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
+tar -xvzf Python-${PYTHON_VERSION}.tgz
+cd Python-${PYTHON_VERSION}
+
+# Build and Install
+./configure \
+    --prefix=/opt/python/${PYTHON_VERSION} \
+    --enable-shared \
+    --enable-optimizations \
+    --enable-ipv6 \
+    LDFLAGS=-Wl,-rpath=/opt/python/${PYTHON_VERSION}/lib,--disable-new-dtags
+
+make
+sudo make install
+
+# pip
+curl -O https://bootstrap.pypa.io/get-pip.py
+sudo /opt/python/${PYTHON_VERSION}/bin/python${PYTHON_MAJOR} get-pip.py
